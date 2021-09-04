aebaa9e1cf112f9bec7c8707f6697509abe02ba0
marwan <5112000>
2021-09-04 21:50:13 +0200

    splsh view

---
+++ lib/modules/splash/splash_screen.dart
@@ -0,0 +1,146 @@
+import 'dart:async';
+
+import 'package:e_commerce_app/shared/components/components.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+
+  final Widget startWidget;
+  const SplashScreen({Key? key, required this.startWidget}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState( startWidget);
+}
+
+
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  final Widget startWidget;
+
+  _SplashScreenState(this.startWidget);
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),(){
+      navigateAndFinish(context, startWidget);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: const AssetImage('assets/images/splash_image.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 100.0,
+              left: 40.0,
+              right: 40.0,
+            ),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                width: double.infinity,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 10,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image:
+                          const AssetImage('assets/images/top_left.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 10,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: const AssetImage(
+                              'assets/images/top_right.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                'SCANNING',
+                style: TextStyle(
+                  fontFamily: 'SF Pro Display',
+                  fontSize: 14,
+                  color: const Color(0xffffffff),
+                  letterSpacing: 8.12,
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              bottom: 100.0,
+              left: 40.0,
+              right: 40.0,
+            ),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: double.infinity,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 10,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image:
+                              const AssetImage('assets/images/bottom_left.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 10,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: const AssetImage(
+                              'assets/images/bottom_right.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
