75e5460d731c9a83110c603dca3ee8f25a17ee37
Mohammed Saad <63466045+mohamedsaad221@users.noreply.github.com>
2021-10-05 17:46:23 +0200

    profile

--- lib/modules/profile/profile_screen.dart
+++ lib/modules/profile/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/shared/components/custom_list_tile.dart';
 import 'package:e_commerce_app/shared/constance.dart';
 import 'package:flutter/material.dart';
 
@@ -6,12 +7,96 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: IconButton(
-        onPressed: () {
-          signOut(context);
-        },
-        icon: Icon(Icons.logout),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children:
+          [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children:
+              [
+                CircleAvatar(
+                  radius: 60.0,
+                  backgroundImage:  NetworkImage(
+                    'https://i.imgur.com/sOzEw8d.png',
+                  ),
+                ),
+                SizedBox(
+                  width: 15.0,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children:
+                  [
+                    Text(
+                      'Mohammed Saad',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w800
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Text(
+                      'mohamedsaad@gmail.com',
+                      style: TextStyle(
+                        fontSize: 11.7,
+                        fontWeight: FontWeight.normal
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:
+                [
+                  CustomListTile(
+                    text: 'Edit Profile',
+                    onPress: (){},
+                    imageName: 'icon_edit_profile.png',
+                  ),
+                  CustomListTile(
+                    text: 'Shipping Address',
+                    onPress: (){},
+                    imageName: 'icon_location.png',
+                  ),
+                  CustomListTile(
+                    text: 'Order History',
+                    onPress: (){},
+                    imageName: 'icon_history.png',
+                  ),
+                  CustomListTile(
+                    text: 'Carts',
+                    onPress: (){},
+                    imageName: 'icon_payment.png',
+                  ),
+                  CustomListTile(
+                    text: 'Notification',
+                    onPress: (){},
+                    imageName: 'icon_alert.png',
+                  ),
+                  CustomListTile(
+                    text: 'Log Out',
+                    onPress: (){
+                      signOut(context);
+                    },
+                    imageName: 'icon_exit.png',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
